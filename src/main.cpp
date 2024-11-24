d378218232af63f3d73fa21d858d174a6a4719f5
Stephen-Laws <sl6114@ic.ac.uk>
2024-11-24 19:26:12 +0000

    Skeleton code template for testing and deployment

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int get42()
+{
+    return 42;
+}
+
+int main()
+{
+    std::cout << "Hello, world" << std::endl;
+    std::cout << get42() << std::endl;
+
+    
+}
---
+++ tests/my_test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+namespace 
+{
+    int Get42(){return 42;}
+}
+
+TEST(TestTopic, TrivialEquality)
+{
+    EXPECT_EQ(Get42(),42);
+}
+
+TEST(TestTopic, MoreEquality)
+{
+    ASSERT_EQ(Get42(), 42) << "Oh no, a mistake!";
+    EXPECT_FLOAT_EQ(23.23F, 23.23F);
+}
